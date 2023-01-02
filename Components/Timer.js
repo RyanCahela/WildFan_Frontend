c430c35ad6e0d411725fce31e8be870abb348d1b
Ryan Cahela <6194542+RyanCahela@users.noreply.github.com>
2023-01-02 11:35:59 -0800

    removed timer custom hook and installed one from npm

--- Components/Timer.js
+++ Components/Timer.js
@@ -1,15 +1,29 @@
-import React, { useState } from "react";
-import App from "../pages/_app";
-import useTimer from "../Hooks/useTimer";
+import React, { useState, useEffect } from "react";
+import { useTimer } from "react-timer-hook";
 
 const Timer = () => {
-  const { time, start, pause, reset, status } = useTimer();
+  const time = new Date();
+  time.setSeconds(time.getSeconds() + 30); //30 second timer
+  const {
+    seconds,
+    minutes,
+    hours,
+    days,
+    isRunning,
+    start,
+    pause,
+    resume,
+    restart,
+  } = useTimer({
+    expiryTimestamp: time,
+    onExpire: () => console.warn("onExpire called"),
+  });
 
   const handleStart = () => {
     start();
   };
 
-  const handleStop = () => {
+  const handlePause = () => {
     pause();
   };
 
@@ -19,10 +33,28 @@ const Timer = () => {
 
   return (
     <div className="text-2xl pt-4">
-      <div className="">Elapsed Time: {time}</div>
-      <button onClick={(e) => handleStart()}>Start</button>
-      <button onClick={(e) => handleStop()}>Stop</button>
-      <button onClick={(e) => handleReset()}>Reset</button>
+      <p>{isRunning ? "running" : "not running"}</p>
+      <div>
+        <div className="">
+          <h3>Elapsed Time:</h3>
+          <div>
+            <span>{minutes}</span>
+            <span>:</span>
+            <span>{seconds}</span>
+          </div>
+        </div>
+      </div>
+      <div className="flex gap-4">
+        <button className="btn" onClick={(e) => handleStart()}>
+          Start
+        </button>
+        <button className="btn" onClick={(e) => handlePause()}>
+          Pause
+        </button>
+        <button className="btn" onClick={(e) => handleReset()}>
+          Reset
+        </button>
+      </div>
     </div>
   );
 };
